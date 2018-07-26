16147c87a3de6e2951ec2ce10e6f4f8a88b6b372
Alan Tavares <alan.am.tavares@gmail.com>
2018-07-26 13:22:00 +0100

    GenericNDK

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,11 +3,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #opencv
-OPENCVROOT:= /home/alantavares/OpenCV-Android/OpenCV-3.3.1-android-sdk
+OPENCVROOT:= OpenCV331
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
+
+#aruco
+
 
 LOCAL_SRC_FILES := main-ndk.cpp
 
